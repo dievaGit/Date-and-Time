aaabe883ed456efbb139e3d2ab4a8b92862afe94
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:23:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FechaHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(" {0}", myValue);
+            Console.ReadKey();
+        }
+    }
+}
